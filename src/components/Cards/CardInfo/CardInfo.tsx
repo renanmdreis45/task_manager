c2a9913b4b6c58694c2fdff3f59cbd22f2d6d1d8
Renan <renanmdreis@alu.ufc.br>
2022-10-03 19:22:21 -0300

    CardInfo

--- src/components/Cards/CardInfo/CardInfo.tsx
+++ src/components/Cards/CardInfo/CardInfo.tsx
@@ -2,3 +2,52 @@ import React, {useEffect, useState} from "react";
 import "./CardInfo.css";
 import CustomInput from "../../UI/CustomInput/CustomInput";
 
+import {ICard} from "../../../interfaces/interface";
+import Card from "../Card";
+
+interface CardioInfoProps {
+    onClose: () => void;
+    card: ICard;
+    groupId: number;
+    updateCard: (groupId: number, cardId: number, card: ICard) => void;
+}
+
+function CardInfo(props: CardioInfoProps) {
+    const {onClose, card, groupId, updateCard} = props;
+    const [cardValues, setCardValues] = useState<ICard>({
+      ...card,
+    });
+
+    const updateDesc = (value: string) => {
+        setCardValues({...card, desc: value})
+    }
+
+    const updateStatus = (status: string) => {
+        setCardValues({...card, state: status})
+    }
+
+    const updatePrazo = (prazo: string) => {
+        if(!prazo) return;
+
+        setCardValues({
+            ...cardValues,
+            prazo,
+        });
+    };
+
+    useEffect(() => {
+        if(updateCard) updateCard(groupId, cardValues.id, cardValues);
+    }, [cardValues])
+
+    return (
+        <div className="card-info">
+            <div className="card-info-box">
+                <div className="card-info-box-title">
+                  <p> Description</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardInfo;
